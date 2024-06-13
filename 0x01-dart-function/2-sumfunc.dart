7b423be9073ac1edc32b207465a786409219e479
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-06-13 10:58:22 -0500

    Messed up a space YAY

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -9,5 +9,5 @@ int sub(int a, int b) {
 String showFunc(int a, int b) {
   int sum = add(a, b);
   int dif = sub(a, b);
-  return 'Add $a + $b = $sum \n Sub $a - $b = $dif';
+  return 'Add $a + $b = $sum \nSub $a - $b = $dif';
 }
