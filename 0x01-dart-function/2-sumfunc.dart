a69357263731f2f62079e4c0a1d7d6444898fb37
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-06-13 10:51:21 -0500

    sumfunc add and sub written

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,13 @@
+int add(int a, int b) {
+  return a + b;
+}
+
+int sub(int a, int b) {
+  return a - b;
+}
+
+String showFunc(int a, int b) {
+  int sum = add(a, b);
+  int sub = sub(a, b);
+  return 'Add $a + $b = $sum \n Sub $a - $b = $sum';
+}
