3df23bb518f73db8e0c06695f5638468a078064e
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-06-18 13:30:38 -0500

    9 and 10. I didn't wanna start 8

---
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -0,0 +1,35 @@
+bool isPalindrome(String s) {
+  if (s.length < 3) {
+    return false;
+  }
+
+  int start = 0;
+  int end = s.length - 1;
+
+  while (start < end) {
+    if (s[start] != s[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
+}
+
+String longestPalindrome(String s) {
+  int maxLength = 0;
+  String longestPalindrome = "";
+
+  if (s.length == 0) {
+    return None;
+  }
+  
+  for (String s in strings) {
+    if (isPalindrome(s) && s.length > maxLength) {
+      maxLength = s.length;
+      longestPalindrome = s;
+    }
+  }
+  return longestPalindrome;
+}
