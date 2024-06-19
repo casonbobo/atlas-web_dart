2412c68a92e5e410fc4d50af8c9c6622eb072ff5
casonbobo <casonsbobo@gmail.com>
2024-06-19 13:31:41 -0500

    class for password

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,17 @@
+class Password{
+  String password = '';
+
+  bool isValid() {
+    bool lengthCheck = password.length >= 8 && password.length <= 16;
+    bool hasUpperCase = password.contains(RegExp(r'[A-Z]'));
+    bool hasLowerCase = password.contains(RegExp(r'[a-z]'));
+    bool hasNumber = password.contains(RegExp(r'[0-9]'));
+
+    return lengthCheck && hasUpperCase && hasLowerCase && hasNumber;
+  }
+
+  @override
+  String toString() {
+    return 'Your password is $password';
+  }
+}
