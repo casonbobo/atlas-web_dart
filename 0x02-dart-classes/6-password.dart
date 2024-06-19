be85f88f1af6532d4c39034077b2bd7d24e8e860
casonbobo <casonsbobo@gmail.com>
2024-06-19 14:56:18 -0500

    THis is maybe 6

---
+++ 0x02-dart-classes/6-password.dart
@@ -0,0 +1,27 @@
+class Password {
+  String _password = '';
+
+  Password({String password = ''}) {
+    _password = password;
+  }
+
+  String get password => _password;
+
+  set password(String value) {
+    _password = value;
+  }
+
+  bool isValid() {
+    bool lengthCheck = _password.length >= 8 && _password.length <= 16;
+    bool hasUpperCase = _password.contains(RegExp(r'[A-Z]'));
+    bool hasLowerCase = _password.contains(RegExp(r'[a-z]'));
+    bool hasNumber = _password.contains(RegExp(r'[0-9]'));
+
+    return lengthCheck && hasUpperCase && hasLowerCase && hasNumber;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
