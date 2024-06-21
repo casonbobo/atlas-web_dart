7de9ab235342cf4227fb0fdd0958190174c9d844
casonbobo <casonsbobo@gmail.com>
2024-06-21 12:52:25 -0500

    I am so tired of this

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -1,11 +1,11 @@
-import 'dart:convert';
+import 'dart:async';
 import '2-util.dart';
 
 Future<void> getUser() async {
   try {
     Map<String, dynamic> userData = jsonDecode(await fetchUser());
     String userID = userData['id'];
-    return userID;
+    print(userID);
   } catch (e) {
      print('error caught: $e');
   }
