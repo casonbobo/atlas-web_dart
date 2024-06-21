1b7075a7eb3a48c18d1aa60fce9ade3ec4e22128
casonbobo <casonsbobo@gmail.com>
2024-06-21 12:32:22 -0500

    ugh. throwing errors in the wrong place

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -4,8 +4,7 @@ import '2-util.dart';
 Future<String> getUser() async {
   try {
     Map<String, dynamic> userData = jsonDecode(await fetchUser());
-    String userID = userData[];
-    return userID;
+    return userData;
   } catch (e) {
     return 'error caught: $e';
   }
