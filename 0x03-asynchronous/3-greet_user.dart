51ceb9944c2e24ad41b4f97f6453fc971ca9e456
casonbobo <casonsbobo@gmail.com>
2024-06-21 13:24:30 -0500

    task 3 i believe

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import '3-util.dart';
+
+Future<String> greetUser() async {
+  try {
+    Map<String, dynamic> userData = jsonDecode(await fetchUser());
+    String username = userData['username'];
+    return 'Hello $username';
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    bool isLoggedIn = await checkCredentials();
+    if (isLoggedIn) {
+      print('There is a user: true');
+      return greetUser();
+    } else {
+      print('There is a user: false');
+      return 'Wrong credentials';
+    }
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
