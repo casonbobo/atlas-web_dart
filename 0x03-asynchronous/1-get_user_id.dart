26cdc00b8717016fba44a2f69cbbb90f757a3b35
casonbobo <casonsbobo@gmail.com>
2024-06-21 12:15:11 -0500

    maybe this is async one. async confuses me lol

---
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+import '1-util.dart';
+
+Future<String> getUserID() async {
+  try {
+    Map<String, dynamic> userData = jsonDecode(await fetchUserData());
+    String userID = userData['id'];
+    return userID;
+  } catch (e) {
+    return 'Error fetching user ID: $e';
+  }
+}
