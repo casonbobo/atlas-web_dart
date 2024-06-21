2e051bd0ca4807fefec410f324e424054507438b
casonbobo <casonsbobo@gmail.com>
2024-06-21 12:33:51 -0500

    ugh typing now??

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -4,7 +4,8 @@ import '2-util.dart';
 Future<String> getUser() async {
   try {
     Map<String, dynamic> userData = jsonDecode(await fetchUser());
-    return userData;
+    Future<String> user = userData
+    return user;
   } catch (e) {
     return 'error caught: $e';
   }
