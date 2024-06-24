4c72c97f7943241a408aadad83f957b88b7d8563
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-06-24 12:30:23 -0500

    MAYBBEEEE

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -6,8 +6,9 @@ Future<double> calculateTotal() async {
     Map<String, dynamic> userData = jsonDecode(await fetchUserData());
     int userID = userData['id'];
 
-    List<Map<String, dynamic>> userOrders = await fetchUserOrders(ordersJson(userID).cast<String>());
-
+    var userOrders = await fetchUserOrders(userId);
+    List<dynamic> orders = convert.jsonDecode(userOrders);
+    
     double totalPrice = 0;
     for (Map<String, dynamic> order in userOrders) {
       String productName = order['product'];
